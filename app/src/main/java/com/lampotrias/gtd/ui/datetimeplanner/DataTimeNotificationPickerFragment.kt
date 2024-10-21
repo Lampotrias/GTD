4ccfc148bfd6441ec307159cc969d9f2a01c60b8
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2024-10-21 09:47:26 +0200

    сделал механизм показа и выбора времени нотификации

--- app/src/main/java/com/lampotrias/gtd/ui/datetimeplanner/DataTimeNotificationPickerFragment.kt
+++ app/src/main/java/com/lampotrias/gtd/ui/datetimeplanner/DataTimeNotificationPickerFragment.kt
@@ -2,21 +2,34 @@ package com.lampotrias.gtd.ui.datetimeplanner
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.setPadding
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
 import com.lampotrias.gtd.databinding.FragmentDataTimeNotificationPickerBinding
 import com.lampotrias.gtd.tools.DrawableUtils
 import com.lampotrias.gtd.tools.OnClickCooldownListener
 import com.lampotrias.gtd.tools.dpToPx
+import com.lampotrias.gtd.ui.datetimeplanner.utils.DateOption
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format
+import kotlinx.datetime.format.char
+import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -49,11 +62,142 @@ class DataTimeNotificationPickerFragment : DialogFragment() {
 
         initializeUI()
 
+        val localTimeFormat =
+            LocalTime.Format {
+                hour()
+                char(':')
+                minute()
+            }
+
+        val localDateTimeFormat =
+            LocalDateTime.Format {
+                dayOfMonth()
+                char('.')
+                monthNumber()
+                char('.')
+                year()
+                char(' ')
+                hour()
+                char(':')
+                minute()
+            }
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
                     viewModel.uiState.collect { uiState ->
-                        binding.finalTime.text = uiState.availableDataTimeModel.toString()
+
+                        binding.btnOk.setOnClickListener(
+                            OnClickCooldownListener {
+                                listener?.onDialogResult(viewModel.uiState.value.selectedDateTime)
+                                dismiss()
+                            },
+                        )
+
+                        binding.formattedDateTime.text =
+                            uiState.selectedDateTime.format(localDateTimeFormat)
+
+                        binding.formattedDateTime.setOnClickListener(
+                            OnClickCooldownListener {
+                                val datePicker =
+                                    MaterialDatePicker.Builder
+                                        .datePicker()
+                                        .setSelection(
+                                            uiState.selectedDateTime
+                                                .toInstant(TimeZone.currentSystemDefault())
+                                                .toEpochMilliseconds(),
+                                        ).setTitleText("Select date")
+                                        .build()
+
+                                datePicker.addOnPositiveButtonClickListener {
+                                    val newDate =
+                                        Instant.fromEpochMilliseconds(it).toLocalDateTime(
+                                            TimeZone.currentSystemDefault(),
+                                        )
+
+                                    val timePicker =
+                                        MaterialTimePicker
+                                            .Builder()
+                                            .apply {
+                                                setHour(uiState.selectedDateTime.hour)
+                                                setMinute(uiState.selectedDateTime.minute)
+                                            }.build()
+
+                                    timePicker.addOnPositiveButtonClickListener {
+                                        val newTime = LocalTime(timePicker.hour, timePicker.minute)
+                                        viewModel.onApplyPickedDateTime(
+                                            LocalDateTime(
+                                                newDate.date,
+                                                newTime,
+                                            ),
+                                        )
+                                    }
+                                    timePicker.show(childFragmentManager, "TIME_PICKER")
+                                }
+                                datePicker.show(
+                                    childFragmentManager,
+                                    "DATE_PICKER",
+                                )
+                            },
+                        )
+
+                        uiState.availableDataTimeModel?.getContentIfNotHandled()?.let { result ->
+                            binding.dateContainer.removeAllViews()
+                            binding.timeContainer.removeAllViews()
+                            result.allTimeIntervals
+                                .forEach { timeInterval ->
+                                    TextView(requireContext())
+                                        .apply {
+                                            text =
+                                                LocalTime
+                                                    .fromSecondOfDay(timeInterval.seconds)
+                                                    .format(localTimeFormat)
+                                            layoutParams =
+                                                LinearLayout
+                                                    .LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+                                                    .apply {
+                                                        gravity = Gravity.CENTER_HORIZONTAL
+                                                    }
+                                            setPadding(requireContext().dpToPx(3))
+                                            setOnClickListener(
+                                                OnClickCooldownListener {
+                                                    viewModel.onTimeSelected(timeInterval)
+                                                },
+                                            )
+                                        }.also {
+                                            binding.timeContainer.addView(it)
+                                        }
+                                }
+
+                            result.availableDateIntervals.forEach { dateInterval ->
+                                TextView(requireContext())
+                                    .apply {
+                                        text =
+                                            when (dateInterval) {
+                                                is DateOption.InTwoWeeks -> "Через неделю"
+                                                is DateOption.NextWeek -> "На следующей неделе"
+                                                is DateOption.OnWeekend -> "На выходных"
+                                                is DateOption.Sunday -> "В воскресенье"
+                                                is DateOption.Today -> "Сегодня"
+                                                is DateOption.Tomorrow -> "Завтра"
+                                            }
+                                        layoutParams =
+                                            LinearLayout
+                                                .LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+                                                .apply {
+                                                    gravity = Gravity.CENTER_HORIZONTAL
+                                                }
+                                        setPadding(requireContext().dpToPx(3))
+                                        setOnClickListener(
+                                            OnClickCooldownListener {
+                                                viewModel.onDateSelected(dateInterval)
+                                            },
+                                        )
+                                    }.also {
+                                        binding.dateContainer.addView(it)
+                                    }
+                            }
+                        }
                     }
                 }
             }
@@ -61,21 +205,15 @@ class DataTimeNotificationPickerFragment : DialogFragment() {
     }
 
     private fun initializeUI() {
-        binding.finalTime.text = "1231"
-
         dialog?.window?.decorView?.background =
             DrawableUtils.createCustomBackground(
                 Color.WHITE,
                 requireContext().dpToPx(DIALOG_CORNER_RADIUS),
             )
 
-        binding.btnOk.setOnClickListener(
-            OnClickCooldownListener {
-                listener?.onDialogResult(viewModel.localTime.value)
-                dismiss()
-            },
-        )
+        binding.btnOk.background = DrawableUtils.createCustomBackground(Color.GREEN, 30f)
 
+        binding.btnCancel.background = DrawableUtils.createCustomBackground(Color.LTGRAY, 30f)
         binding.btnCancel.setOnClickListener(
             OnClickCooldownListener {
                 dismiss()
@@ -89,6 +227,5 @@ class DataTimeNotificationPickerFragment : DialogFragment() {
 
     companion object {
         private const val DIALOG_CORNER_RADIUS = 12f
-        private const val TITLE_CORNER_RADIUS = 6f
     }
 }
--- app/src/main/java/com/lampotrias/gtd/ui/datetimeplanner/DataTimeNotificationPickerViewModel.kt
+++ app/src/main/java/com/lampotrias/gtd/ui/datetimeplanner/DataTimeNotificationPickerViewModel.kt
@@ -4,19 +4,24 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lampotrias.gtd.domain.usecases.GetAvailableNotifyDateTimeUseCase
+import com.lampotrias.gtd.tools.SingleEvent
 import com.lampotrias.gtd.ui.datetimeplanner.utils.AvailableDataTimeModel
+import com.lampotrias.gtd.ui.datetimeplanner.utils.DateOption
+import com.lampotrias.gtd.ui.datetimeplanner.utils.TimeOptions
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 
 data class DataTimeNotificationPickerScreenUI(
     val isLoading: Boolean = false,
-    val availableDataTimeModel: AvailableDataTimeModel = AvailableDataTimeModel(),
+    val availableDataTimeModel: SingleEvent<AvailableDataTimeModel>? = null,
+    val openPickerEvent: SingleEvent<LocalDateTime>? = null,
+    val selectedDateTime: LocalDateTime,
 )
 
 class DataTimeNotificationPickerViewModel(
@@ -24,27 +29,80 @@ class DataTimeNotificationPickerViewModel(
     getAvailableNotifyDateTimeUseCase: GetAvailableNotifyDateTimeUseCase,
     currentLocalTime: LocalDateTime,
 ) : ViewModel() {
-    private val _isLoading = MutableStateFlow(false)
-    val localTime: MutableStateFlow<LocalDateTime> = MutableStateFlow(currentLocalTime)
+    private var wasInitialized = false
+
+    private val _innerScreenUI =
+        MutableStateFlow(
+            DataTimeNotificationPickerScreenUI(
+                selectedDateTime = currentLocalTime,
+            ),
+        )
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val uiState: StateFlow<DataTimeNotificationPickerScreenUI> =
-        localTime
-            .flatMapConcat { localTime ->
-                getAvailableNotifyDateTimeUseCase.invoke(localTime)
-            }.combine(_isLoading) { availableDataTimeModel, isLoading ->
-                DataTimeNotificationPickerScreenUI(
-                    isLoading = isLoading,
-                    availableDataTimeModel = availableDataTimeModel,
-                )
-            }.stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(WHILE_SUBSCRIBED_TIMEOUT),
-                initialValue = DataTimeNotificationPickerScreenUI(),
+        combine(
+            getAvailableNotifyDateTimeUseCase.invoke(currentLocalTime),
+            _innerScreenUI,
+        ) { availableDataTimeModel, innerScreenUI ->
+            DataTimeNotificationPickerScreenUI(
+                availableDataTimeModel =
+                    if (wasInitialized) {
+                        null
+                    } else {
+                        wasInitialized = true
+                        SingleEvent(
+                            availableDataTimeModel,
+                        )
+                    },
+                selectedDateTime =
+                    if (wasInitialized) {
+                        innerScreenUI.selectedDateTime
+                    } else {
+                        LocalDateTime(
+                            availableDataTimeModel.availableDateIntervals.first().date,
+                            LocalTime.fromSecondOfDay(
+                                availableDataTimeModel.availableTodayTimeIntervals.firstOrNull()?.seconds
+                                    ?: availableDataTimeModel.allTimeIntervals.first().seconds,
+                            ),
+                        )
+                    },
             )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(WHILE_SUBSCRIBED_TIMEOUT),
+            initialValue =
+                DataTimeNotificationPickerScreenUI(
+                    selectedDateTime = currentLocalTime,
+                ),
+        )
 
-    fun updateTime(dateTime: LocalDateTime) {
-        localTime.value = dateTime
+    fun onApplyPickedDateTime(dateTime: LocalDateTime) {
+        _innerScreenUI.value =
+            _innerScreenUI.value.copy(
+                selectedDateTime = dateTime,
+            )
+    }
+
+    fun onTimeSelected(timeOptions: TimeOptions) {
+        _innerScreenUI.value =
+            _innerScreenUI.value.copy(
+                selectedDateTime =
+                    LocalDateTime(
+                        uiState.value.selectedDateTime.date,
+                        LocalTime.fromSecondOfDay(timeOptions.seconds),
+                    ),
+            )
+    }
+
+    fun onDateSelected(dateInterval: DateOption) {
+        _innerScreenUI.value =
+            _innerScreenUI.value.copy(
+                selectedDateTime =
+                    LocalDateTime(
+                        dateInterval.date,
+                    uiState.value.selectedDateTime.time,
+                ),
+            )
     }
 
     companion object {
